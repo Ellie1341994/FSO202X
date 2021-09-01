3120fee4e78c924faade6869284a3055f19b5595
ellie <eli1341994@gmail.com>
2021-09-01 08:42:54 -0300

    Exercise 1.8 Refactor statistics as a component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 
 const baseFeedbackData = { good: 0, bad: 0, neutral: 0 };
+
 const Title = ({ text }) => <h1>{text}</h1>;
 const Feedback = ({ type, value }) => <p>{type + ": " + value}</p>;
-const UserFeedback = ({ data }) => {
+const Statistics = ({ data }) => {
+  const all = data.good + data.bad + data.neutral;
+  const average = (data.good - data.bad) / (all || 1);
+  const positivePortion = (100 * data.good) / (all || 1);
   return (
     <div>
       <Title text="Statitics" />
-      <Feedback type="good" value={data.good} />
-      <Feedback type="bad" value={data.bad} />
-      <Feedback type="neutral" value={data.neutral} />
+      <Feedback type="Good" value={data.good} />
+      <Feedback type="Bad" value={data.bad} />
+      <Feedback type="Neutral" value={data.neutral} />
+      <Feedback type="All" value={all} />
+      <Feedback type="Average" value={average} />
+      <Feedback type="Positive" value={positivePortion} />
     </div>
   );
 };
@@ -35,7 +42,7 @@ const App = () => {
   return (
     <>
       <FeedbackPanel data={feedback} action={setFeedback} />
-      <UserFeedback data={feedback} />
+      <Statistics data={feedback} />
     </>
   );
 };
